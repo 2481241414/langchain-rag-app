1613a865830ae8a8b71590117ebe5afae81129a2
lgqnb <2481241414@qq.com>
2024-08-15 10:44:59 +0800

    2024-08-14

--- pre-requirements.sh
+++ pre-requirements.sh
@@ -1,4 +1,3 @@
-apt-get update && apt-get install -y wget tar build-essential
 wget https://www.sqlite.org/2024/sqlite-autoconf-3460100.tar.gz
 tar -xvzf sqlite-autoconf-3460100.tar.gz
 cd sqlite-autoconf-3460100
